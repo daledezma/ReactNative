c87727ac8ccdc831966603174c358e9050737964
Daniel Ledezma <dledezma@me.com>
2020-05-01 17:08:14 -0500

    Loading component added

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,8 @@ import { ScrollView, FlatList, Text } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+
 
 const mapStateToProps = state => {
     return {
@@ -45,6 +47,29 @@ class About extends Component {
                     leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             )
+        };
+
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
         }
         return (
             <ScrollView>
